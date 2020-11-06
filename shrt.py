9eca0650afc9e926ceda3fa5a901642006a967e9
Krasius <DarkRisingForce@Gmail.com>
2020-11-07 00:03:49 +0200

    refactored script; added config

--- shrt.py
+++ shrt.py
@@ -2,31 +2,22 @@
 Url shortener script using bitly API
 """
 import argparse
+import configparser
 import requests
 
 # refactoring
 
 
-def shorten_link(username, password, token, url="https://www.thepythoncode.com/topic/using-apis-in-python"):
+def shorten_link(token, guid, url="https://www.thepythoncode.com/topic/using-apis-in-python"):
     # construct the request headers with authorization
-    headers = {"Authorization": f"Bearer {access_token}"}
-
-    # get the group UID associated with our account
-    groups_res = requests.get(
-        "https://api-ssl.bitly.com/v4/groups", headers=headers)
-    if groups_res.status_code == 200:
-        # if response is OK, get the GUID
-        groups_data = groups_res.json()['groups'][0]
-        guid = groups_data['guid']
-        # print(guid)
-    else:
-        print("[!] Cannot get GUID, exiting...")
-        exit()
-
-
+    headers = {"Authorization": f"Bearer {token}",
+               'Content-Type': 'application/json',}
+               
     # make the POST request to get shortened URL for `url`
-    shorten_res = requests.post("https://api-ssl.bitly.com/v4/shorten",
-                                json={"group_guid": guid, "long_url": url}, headers=headers)
+    json = {"group_guid": guid, "long_url": url}
+    shorten_res = requests.post("https://api-ssl.bitly.com/v4/shorten", 
+            headers=headers, json=json)
+    # print(shorten_res.status_code)
     if shorten_res.status_code == 200:
         # if response is OK, get the shortened URL
         link = shorten_res.json().get("link")
@@ -36,14 +27,19 @@ def shorten_link(username, password, token, url="https://www.thepythoncode.com/t
 
 
 if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read('etc/config.ini')
+    
     # account info (Temporary)
-    username = 'o_4fns29md36'
-    password = '.A&K5.9uT8W@n65'
-    access_token = 'e4a7325b97e274fd7c86b05d6bb5d59bf88268c9'
+    username = config.get('configuration', 'username')
+    password = config.get('configuration', 'password')
+    access_token = config.get('configuration', 'access_token')
+    guid = config.get('configuration', 'group_id')
     # the URL you want to shorten
     url = "https://www.thepythoncode.com/topic/using-apis-in-python"
-
+    
     parser = argparse.ArgumentParser(description='URL Shortener v1')
     parser.add_argument('link', help='long url to be shortened')
     args = parser.parse_args()
-    shorten_link(username, password, access_token, args.link)
+
+    shorten_link(access_token, guid, args.link)
